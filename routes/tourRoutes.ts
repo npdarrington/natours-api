4d6136fa5d47965bd1e8bc12806d77714e7cf4cd
Nathan Darrington <npdarrington@icloud.com>
2021-10-29 12:26:30 -0600

    Define routes for tours and export

---
+++ routes/tourRoutes.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import {
+  getAllTours,
+  createTour,
+  getTour,
+  updateTour,
+  deleteTour,
+} from '../controllers/tourController';
+
+const router = express.Router();
+router.route('/').get(getAllTours).post(createTour);
+router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
+export default router;
