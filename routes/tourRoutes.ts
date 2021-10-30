d5ee58b9b4e0feeeddd7e19dcd026411aecd6418
Nathan Darrington <npdarrington@icloud.com>
2021-10-29 18:09:21 -0600

    Implement validateID on tourRoutes

--- routes/tourRoutes.ts
+++ routes/tourRoutes.ts
@@ -5,9 +5,11 @@ import {
   getTour,
   updateTour,
   deleteTour,
+  validateID,
 } from '../controllers/tourController';
 
 const router = express.Router();
+router.param('id', validateID);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
 export default router;
