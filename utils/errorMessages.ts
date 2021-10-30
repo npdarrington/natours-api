12e199f55730fd3f24076cc0a27decb831acf746
Nathan Darrington <npdarrington@icloud.com>
2021-10-29 18:09:38 -0600

    Create ErrorMessages enum

---
+++ utils/errorMessages.ts
@@ -0,0 +1,5 @@
+export enum ErrorMessages {
+  INVALID_ID = 'Invalid ID.',
+  TOUR_DOES_NOT_EXIST = 'Tour does not exist.',
+  SERVER_OFFLINE = 'Server is offline. Contact system administrator.',
+}
