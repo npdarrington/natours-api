8437975ae3549371b042af96ebcea2ae736ef50a
Nathan Darrington <npdarrington@icloud.com>
2021-10-30 13:37:25 -0600

    Create tour schema and export model

---
+++ models/tourModel.ts
@@ -0,0 +1,25 @@
+import { Schema, model } from 'mongoose';
+
+interface Tour {
+  name: string;
+  rating?: number;
+  price: number;
+}
+
+const tourSchema = new Schema<Tour>({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+export const TourModel = model<Tour>('Tour', tourSchema);
