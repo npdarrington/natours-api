36186fa7af1e0b0b5af9952745ee3255167a5d09
Nathan Darrington <npdarrington@icloud.com>
2021-10-30 11:42:46 -0600

    Configure dbConn and test success conn

--- server.ts
+++ server.ts
@@ -1,7 +1,16 @@
 import { app } from './app';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
+const dbConn: string | undefined = process.env.DATABASE?.replace(
+  '<PASSWORD>',
+  process.env.DB_PASSWORD!
+);
+
+if (dbConn) {
+  mongoose.connect(dbConn).then(() => console.log('DB connection successful'));
+}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`App running on Port ${port}`));
