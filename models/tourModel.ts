4f2de6c1d49d7af91cfebc4ba60d2e0c33b363ba
Nathan Darrington <npdarrington@icloud.com>
2021-10-30 22:28:31 -0600

    Added additional properties to Tour interface

--- models/tourModel.ts
+++ models/tourModel.ts
@@ -1,9 +1,20 @@
 import { Schema, model, Document } from 'mongoose';
 
-interface Tour extends Document {
+export interface Tour extends Document {
   name: string;
-  rating?: number;
+  duration: number;
+  maxGroupSize: number;
+  difficulty: string;
+  ratingsAverage?: number;
+  ratingsQuantity?: number;
   price: number;
+  priceDiscount?: number;
+  summary: string;
+  description?: string;
+  imageCover: string;
+  images?: string[];
+  createdAt: Date;
+  startDates: Date[];
 }
 
 const tourSchema = new Schema<Tour>({
@@ -11,15 +22,53 @@ const tourSchema = new Schema<Tour>({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
+    trim: true,
   },
-  rating: {
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration'],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size'],
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty'],
+  },
+  ratingsAverage: {
     type: Number,
     default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description'],
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a cover image'],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 export const TourModel = model<Tour>('Tour', tourSchema);
