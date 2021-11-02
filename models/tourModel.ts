90f1164657a255f6cfde453b59d7753bf382442b
Nathan Darrington <npdarrington@icloud.com>
2021-11-02 08:23:58 -0600

    Move filter functionality to filter method on APIFeatures.
    APIFeatures is a Generic Class that takes in a type. This type will be the kind of data that this APIFeatures will be an instance of. For example, when working with the TourModel, it will be expecting the Tour interface to know what kind of data will be expected in the Query. It accepts two parameters, the Query (Expecting a resultType of a Tour array and a DocType of a Tour) and the query string (specified with the QueryArgs type).

--- models/tourModel.ts
+++ models/tourModel.ts
@@ -1,6 +1,6 @@
 import { Schema, model } from 'mongoose';
 
-interface Tour {
+export interface Tour {
   name: string;
   duration: number;
   maxGroupSize: number;
--- utils/APIFeatures.ts
+++ utils/APIFeatures.ts
@@ -1,5 +1,24 @@
-import { Model } from 'mongoose';
+import { Query } from 'mongoose';
+import { ParsedQs } from 'qs';
 
-export class APIFeatures<T extends Model<T>> {
-  constructor(public query: T, public queryString: { [key: string]: string }) {}
+type QueryArgs = string | string[] | ParsedQs | ParsedQs[] | undefined;
+
+export class APIFeatures<T> {
+  constructor(
+    public query: Query<T[], T>,
+    public queryString: { [key: string]: QueryArgs }
+  ) {}
+
+  filter(): void {
+    const queryObj = { ...this.queryString };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let advFilter = JSON.stringify(queryObj);
+    advFilter = advFilter.replace(
+      /\b(gte|gt|lte|lt)\b/g,
+      (match) => `$${match}`
+    );
+    this.query.find(JSON.parse(advFilter));
+  }
 }
