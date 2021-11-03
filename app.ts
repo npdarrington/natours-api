2b7f99a6cae0104b667fe531d58da2c8110a606e
Nathan Darrington <npdarrington@icloud.com>
2021-11-03 13:57:04 -0600

    Use globalErrorHandler if any error hits

--- app.ts
+++ app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 
+import { globalErrorHandler } from './controllers/errorController';
 import tourRouter from './routes/tourRoutes';
 
 export const app = express();
 app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);
+app.use(globalErrorHandler);
