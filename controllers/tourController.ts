bec0a5256021637721122047581ec999ea25dd37
Nathan Darrington <npdarrington@icloud.com>
2021-11-03 18:42:14 -0600

    Wrap all methods with asyncTryCatch and remove try-catch blocks

--- controllers/tourController.ts
+++ controllers/tourController.ts
@@ -1,19 +1,20 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 import { TourModel, Tour } from '../models/tourModel';
 import { APIFeatures } from '../utils/APIFeatures';
+import { asyncTryCatch } from '../utils/asyncTryCatch';
 
 import { ResponseStatus } from '../utils/responseStatus';
-import { ErrorMessages } from '../utils/errorMessages';
 
-export const getAllTours = async (request: Request, response: Response) => {
-  try {
+export const getAllTours = asyncTryCatch(
+  async (request: Request, response: Response, next: NextFunction) => {
     const features = new APIFeatures<Tour>(TourModel.find(), request.query)
       .filter()
       .sort()
       .limitFields()
       .paginate();
     const tours = await features.query;
+
     response.status(200).json({
       status: ResponseStatus.SUCCESS,
       results: tours.length,
@@ -21,50 +22,37 @@ export const getAllTours = async (request: Request, response: Response) => {
         tours,
       },
     });
-  } catch (error) {
-    response.status(500).json({
-      status: ResponseStatus.FAILURE,
-      message: ErrorMessages.SERVER_OFFLINE,
-    });
   }
-};
+);
 
-export const getTour = async (request: Request, response: Response) => {
-  try {
+export const getTour = asyncTryCatch(
+  async (request: Request, response: Response, next: NextFunction) => {
     const tour = TourModel.findById(request.params.id);
+
     response.status(200).json({
       status: ResponseStatus.SUCCESS,
       data: {
         tour,
       },
     });
-  } catch (error) {
-    response.status(404).json({
-      status: ResponseStatus.FAILURE,
-      message: ErrorMessages.TOUR_DOES_NOT_EXIST,
-    });
   }
-};
+);
 
-export const createTour = async (request: Request, response: Response) => {
-  try {
+export const createTour = asyncTryCatch(
+  async (request: Request, response: Response, next: NextFunction) => {
     const tour = await TourModel.create(request.body);
+
     response.status(201).json({
       status: ResponseStatus.SUCCESS,
       data: {
         tour,
       },
     });
-  } catch (error) {
-    response.status(400).json({
-      status: ResponseStatus.FAILURE,
-      message: ErrorMessages.SERVER_OFFLINE,
-    });
   }
-};
+);
 
-export const updateTour = async (request: Request, response: Response) => {
-  try {
+export const updateTour = asyncTryCatch(
+  async (request: Request, response: Response, next: NextFunction) => {
     const tour = await TourModel.findByIdAndUpdate(
       request.params.id,
       request.body,
@@ -80,17 +68,13 @@ export const updateTour = async (request: Request, response: Response) => {
         tour,
       },
     });
-  } catch (error) {
-    response.status(400).json({
-      status: ResponseStatus.FAILURE,
-      message: ErrorMessages.SERVER_OFFLINE,
-    });
   }
-};
+);
 
-export const deleteTour = async (request: Request, response: Response) => {
-  try {
+export const deleteTour = asyncTryCatch(
+  async (request: Request, response: Response, next: NextFunction) => {
     const result = await TourModel.findByIdAndDelete(request.params.id);
+
     if (result) {
       response.status(204).json({
         status: ResponseStatus.SUCCESS,
@@ -98,10 +82,5 @@ export const deleteTour = async (request: Request, response: Response) => {
     } else {
       throw Error;
     }
-  } catch (error) {
-    response.status(400).json({
-      status: ResponseStatus.FAILURE,
-      message: ErrorMessages.SERVER_OFFLINE,
-    });
   }
-};
+);
