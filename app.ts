c13bf056e062b3ac8718ad603ea7955fe38fc35a
Nathan Darrington <npdarrington@icloud.com>
2021-11-03 18:40:03 -0600

    Create an error handler route if no routes match

--- app.ts
+++ app.ts
@@ -1,10 +1,19 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 
 import { globalErrorHandler } from './controllers/errorController';
+import { AppErrorHandler } from './utils/AppErrorHandler';
 import tourRouter from './routes/tourRoutes';
 
 export const app = express();
 app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);
+app.use('*', (req: Request, res: Response, next: NextFunction) => {
+  next(
+    AppErrorHandler.invokeError(
+      `Can't find ${req.originalUrl} on this server!`,
+      404
+    )
+  );
+});
 app.use(globalErrorHandler);
