d17569df127d01103dc5f5229f2cae2c30d85319
Nathan Darrington <npdarrington@icloud.com>
2021-11-08 10:20:17 -0700

    Create signup post route and include in app

--- app.ts
+++ app.ts
@@ -3,11 +3,13 @@ import express, { Request, Response, NextFunction } from 'express';
 import { globalErrorHandler } from './controllers/errorController';
 import { AppErrorHandler } from './utils/AppErrorHandler';
 import tourRouter from './routes/tourRoutes';
+import userRouter from './routes/userRoutes';
 
 export const app = express();
 app.use(express.json());
 
 app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 app.use('*', (req: Request, res: Response, next: NextFunction) => {
   next(
     AppErrorHandler.invokeError(
