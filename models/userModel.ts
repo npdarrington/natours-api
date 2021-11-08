1f552a662fb9fb347b9d1055b788c49e24d85880
Nathan Darrington <npdarrington@icloud.com>
2021-11-07 20:50:04 -0700

    Create userSchema with error handling and UserModel

---
+++ models/userModel.ts
@@ -0,0 +1,43 @@
+import { Schema, model } from 'mongoose';
+import validator from 'validator';
+
+export interface User {
+  name: string;
+  email: string;
+  photo?: string;
+  password: string;
+  passwordConfirm?: string;
+}
+
+const userSchema = new Schema<User>({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowercase: true,
+    trim: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: {
+    type: String,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, 'A user must have a valid password'],
+    trim: true,
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'A user must verify their passwords match'],
+    trim: true,
+  },
+});
+
+export const UserModel = model<User>('User', userSchema);
